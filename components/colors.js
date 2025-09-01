a674b6c3f31c00de08243876ddc3efd1a476756f
Stoneandresin <aaron@stoneandresin.com>
2025-09-01 07:45:03 -0700

    Add Vuba color dataset with direct product links and images

---
+++ components/colors.js
@@ -0,0 +1,165 @@
+export const vubaColors = [
+  // NEW blends from the first page of the Vuba Americas collection
+  {
+    name: 'Alaskan Tundra',
+    href: 'https://vuba-stone.com/products/colorado-1',
+    image: 'https://vuba-stone.com/cdn/shop/files/Alaskan_Tundra_Blend_14.jpg?v=1748517398',
+  },
+  {
+    name: 'Coral Springs',
+    href: 'https://vuba-stone.com/products/yosemite',
+    image: 'https://vuba-stone.com/cdn/shop/files/Coral_Springs_1.png?v=1748518015',
+  },
+  {
+    name: 'Idaho Falls',
+    href: 'https://vuba-stone.com/products/joshua-tree',
+    image: 'https://vuba-stone.com/cdn/shop/files/Idaho_Falls_1.png?v=1748518518',
+  },
+  {
+    name: 'Juneau',
+    href: 'https://vuba-stone.com/products/juneau',
+    image: 'https://vuba-stone.com/cdn/shop/files/Juneau_2.png?v=1748519907',
+  },
+  {
+    name: 'Kaimu Beach',
+    href: 'https://vuba-stone.com/products/kaimu-beach',
+    image: 'https://vuba-stone.com/cdn/shop/files/KaimuBeach_2.png?v=1748520000',
+  },
+  {
+    name: 'Nevada',
+    href: 'https://vuba-stone.com/products/nevada',
+    image: 'https://vuba-stone.com/cdn/shop/files/Nevada_1.png?v=1748520360',
+  },
+  {
+    name: 'Santa Monica',
+    href: 'https://vuba-stone.com/products/santa-monica',
+    image: 'https://vuba-stone.com/cdn/shop/files/SantaMonica_1.png?v=1748520873',
+  },
+  {
+    name: 'Santo Domingo',
+    href: 'https://vuba-stone.com/products/santo-domingo',
+    image: 'https://vuba-stone.com/cdn/shop/files/Santo_Domingo_1.png?v=1748520933',
+  },
+  {
+    name: 'Savannah',
+    href: 'https://vuba-stone.com/products/savannah',
+    image: 'https://vuba-stone.com/cdn/shop/files/Savannah_1.png?v=1748521006',
+  },
+  // Core blends
+  {
+    name: 'Ellis Island',
+    href: 'https://vuba-stone.com/products/ellis-island',
+    image: 'https://vuba-stone.com/cdn/shop/files/EllisIsland.jpg?v=1738573448',
+  },
+  {
+    name: 'Poseidon',
+    href: 'https://vuba-stone.com/products/poseidon',
+    image: 'https://vuba-stone.com/cdn/shop/files/Poseidon_b4daa220-16bf-4598-9d14-163e0a2eb543.jpg?v=1738573363',
+  },
+  {
+    name: 'Key West',
+    href: 'https://vuba-stone.com/products/key-west',
+    image: 'https://vuba-stone.com/cdn/shop/files/KeyWest.jpg?v=1738573522',
+  },
+  {
+    name: 'Palazzo',
+    href: 'https://vuba-stone.com/products/palazzo-1',
+    image: 'https://vuba-stone.com/cdn/shop/files/Palazzo_05778205-57a6-48e4-a7be-8d2f030b1311.jpg?v=1738573584',
+  },
+  {
+    name: 'Rio Grande',
+    href: 'https://vuba-stone.com/products/rio-grande',
+    image: 'https://vuba-stone.com/cdn/shop/files/RioGrande.jpg?v=1738573543',
+  },
+  {
+    name: 'Ipanema Beach',
+    href: 'https://vuba-stone.com/products/ipanema-beach',
+    image: 'https://vuba-stone.com/cdn/shop/files/IpanemaBeach_33e848c2-5ab9-40f9-97fd-a09500befe27.jpg?v=1738573334',
+  },
+  {
+    name: 'The Burj',
+    href: 'https://vuba-stone.com/products/the-burj',
+    image: 'https://vuba-stone.com/cdn/shop/files/TheBurj.jpg?v=1738573418',
+  },
+  {
+    name: 'Milano Marble',
+    href: 'https://vuba-stone.com/products/milano-marble-1',
+    image: 'https://vuba-stone.com/cdn/shop/files/MilanoMarble_a1935bf6-7774-4c79-91bc-ccf564f9dc07.jpg?v=1738573595',
+  },
+  {
+    name: 'Manhattan',
+    href: 'https://vuba-stone.com/products/manhattan',
+    image: 'https://vuba-stone.com/cdn/shop/files/Manhattan_4f8d833e-8fde-4f4d-b4de-b5953f6dac18.jpg?v=1738573532',
+  },
+  {
+    name: 'Tuscan',
+    href: 'https://vuba-stone.com/products/tuscan',
+    image: 'https://vuba-stone.com/cdn/shop/files/tuscan_068e2909-4ab4-4fb1-92d0-5b9b4171c91d.png?v=1738573385',
+  },
+  {
+    name: 'Tennessee',
+    href: 'https://vuba-stone.com/products/tennessee',
+    image: 'https://vuba-stone.com/cdn/shop/files/Tennessee_600b0e75-3254-4df9-880c-16b6b781adf0.jpg?v=1738573574',
+  },
+  {
+    name: 'China Town',
+    href: 'https://vuba-stone.com/products/china-town',
+    image: 'https://vuba-stone.com/cdn/shop/files/ChinaTown.jpg?v=1738573564',
+  },
+  {
+    name: 'Versailles',
+    href: 'https://vuba-stone.com/products/versailles',
+    image: 'https://vuba-stone.com/cdn/shop/files/Versailles.jpg?v=1738573553',
+  },
+  {
+    name: 'Sistine Chapel',
+    href: 'https://vuba-stone.com/products/sistine-chapel',
+    image: 'https://vuba-stone.com/cdn/shop/files/SistineChapel_eaf07e6b-d55b-4781-ba16-81230b6c438a.jpg?v=1738573500',
+  },
+  {
+    name: 'Breckenridge',
+    href: 'https://vuba-stone.com/products/breckenridge',
+    image: 'https://vuba-stone.com/cdn/shop/files/Breckenridge_f8a3c9c7-67eb-44d6-aa8c-2b9349b2163e.jpg?v=1738573490',
+  },
+  // Additional blends from the second page of the Vuba Americas collection
+  {
+    name: 'Lake Tahoe',
+    href: 'https://vuba-stone.com/products/lake-tahoe',
+    image: 'https://vuba-stone.com/cdn/shop/files/LakeTahoe.jpg?v=1738573396',
+  },
+  {
+    name: 'Big Sur',
+    href: 'https://vuba-stone.com/products/big-sur',
+    image: 'https://vuba-stone.com/cdn/shop/files/BigSur.jpg?v=1738573469',
+  },
+  {
+    name: 'Caño Cristales',
+    href: 'https://vuba-stone.com/products/cano-cristales',
+    image: 'https://vuba-stone.com/cdn/shop/files/CanoCristales.jpg?v=1738573512',
+  },
+  {
+    name: 'Arc de Triomphe',
+    href: 'https://vuba-stone.com/products/arc-de-triomphe',
+    image: 'https://vuba-stone.com/cdn/shop/files/ArcdeTriomphe.jpg?v=1738573438',
+  },
+  {
+    name: 'Monticello',
+    href: 'https://vuba-stone.com/products/monticello',
+    image: 'https://vuba-stone.com/cdn/shop/files/Monticello.jpg?v=1738573428',
+  },
+  {
+    name: 'Rio Medina',
+    href: 'https://vuba-stone.com/products/rio-medina',
+    image: 'https://vuba-stone.com/cdn/shop/files/RioMedina.jpg?v=1738573374',
+  },
+  {
+    name: 'Pont du Gard',
+    href: 'https://vuba-stone.com/products/pont-du-gard',
+    image: 'https://vuba-stone.com/cdn/shop/files/PontduGard.jpg?v=1738573351',
+  },
+  {
+    name: 'Monument Valley',
+    href: 'https://vuba-stone.com/products/monument-valley',
+    image: 'https://vuba-stone.com/cdn/shop/files/MonumentValley.jpg?v=1738573459',
+  },
+];
