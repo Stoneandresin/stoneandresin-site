9144d0ab1e68ca27e63eecff4f8d4181a0618e3a
Stoneandresin <aaron@stoneandresin.com>
2025-09-04 13:48:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Generate pages with a trailing slash so that both `/about` and `/about/` work.
+  trailingSlash: true,
   experimental: {
     typedRoutes: true,
   },
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'vuba-stone.com',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'cdn.shopify.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'vuba-stone.com', pathname: '/**' },
     ],
   },
 };
